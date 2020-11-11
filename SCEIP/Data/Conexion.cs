7200a6c46509f9fb47a66154fb3bd913b4fae195
BaldomeroUnach <53806348+BaldomeroUnach@users.noreply.github.com>
2020-11-11 15:53:52 -0600

    Add files via upload

---
+++ SCEIP/Data/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using MySql.Data.MySqlClient;
+
+
+namespace Data
+{
+    public class Conexion
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=estanciainfantil; Uid=root; pwd=Baldo_Uno_1;");
+            conectar.Open();
+            return conectar;
+        }
+
+        public class Conex
+        {
+
+            public static MySqlConnection ObtenerConex()
+            {
+                MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; Uid=root; pwd=Baldo_Uno_1;");
+                conectar.Open();
+                return conectar;
+            }
+
+        }
+    }
+}
